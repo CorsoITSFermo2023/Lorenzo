9839e5dcf9ef7169718631ef5fb930075e5b4752
Lorenzo <iachini.lorenzo@itssmartacademy.it>
2023-05-12 17:53:08 +0200

    ho creato il file

---
+++ esercizio.js
@@ -0,0 +1,13 @@
+
+let prodotto= {
+    id: 1,
+    prezzo:10,
+    nome:'videogioco',
+    categorie:['sparatutto', 'online']
+}
+console.log(prodotto.categorie[0]);
+const carello = new Array[
+    prodotto.categorie[i]
+];
+console.log(carello);
+
